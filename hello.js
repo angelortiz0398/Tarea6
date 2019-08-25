339cd4c6afac7bc3b7d8b4ee1c007a2ac70e2371
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-08-25 10:31:29 -0500

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+<html>
+ <head><title>Mi primer codigo JavaScript</title></head>
+  <body>
+ 
+  <h1>hello</h1>
+ 
+   <script>
+     document.write("Hola Mundo");
+   </script>
+ 
+  </body>
+</html>
