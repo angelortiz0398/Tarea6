0480deeb109ccd7618e7e471c4c8122cfab080d0
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-08-31 23:15:39 -0500

    Create Absoluto.js

---
+++ Absoluto/Absoluto.js
@@ -0,0 +1,15 @@
+/*
+* Absoluto.js
+* absolute calc
+*/
+const abs(num){
+  if(num < 0)
+    return num *-1
+  else
+    return num
+}
+void main(){
+  console.log(abs(-4))
+  console.log(abs(0))
+  console.log(abs(9))
+}
