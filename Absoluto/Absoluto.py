5c753fc59000d85c86985cc09d9714af368431b1
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-08-31 22:36:37 -0500

    Add files via upload

---
+++ Absoluto/Absoluto.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def abs(numero):
+    return numero * -1 if numero < 0 else numero
+def main():
+    print(abs(-4)) # 4
+    print(abs(0)) # 0
+    print(abs(-9)) # 9
+
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
