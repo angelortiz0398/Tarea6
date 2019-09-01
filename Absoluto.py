408bc03fa42a9a39af76f4ee39125d37a99e1f30
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-08-31 22:28:42 -0500

    Create Absoluto.py

---
+++ Absoluto.py
@@ -0,0 +1,10 @@
+def abs(numero):
+    return numero * -1 if numero < 0 else numero
+def main():
+    print(abs(-4)) # 4
+    print(abs(0)) # 0
+    print(abs(-9)) # 9
+
+
+if __name__ == '__main__':
+    main()
