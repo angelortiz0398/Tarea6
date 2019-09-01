eea67101876c842d2f342f5639f07eec318c60b4
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-08-31 23:20:57 -0500

    Add files via upload
    Functions in Python & Javascript

---
+++ Tarea 7 -Funciones-/Functions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def add(a,b):
+	return a + b
+def sustract(a,b):
+	return a - b
+def multiply(a,b):
+	return a * b
+def divide(a,b):
+	return a / b
+
+#print(add(2,4))
+
+
+
+
+
