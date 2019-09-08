6c466568c8a2560cfca65c364492a9afd118443b
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-09-07 20:45:23 -0500

    Update Chicharronera.py

--- Chicharronera/Chicharronera.py
+++ Chicharronera/Chicharronera.py
@@ -3,27 +3,23 @@
 * Solution for x1, x2
 * When I. Term = 0
 """
-
+import math
 
 def Abs(num):
     return num * -1 if num < 0 else num
 
 def CuadraticE(a,b,c):
-    
-    if((b**2 - 4*a*c) <= 0):
-        d = (b**2 - 4*a*c)**(1/2)
-        e = 2*a
-        x1 = (b + d)/e
-        x2 = (b - d)/e
-        return x1, x2 
-        
+    div = 2 * float(a)
+    disc = b * b - (4 * a * c)
+    if disc < 0:
+        x1 = str(-b / div) + " + " + str(math.sqrt(dis * -1) / div) + "i"
+        x2 = str(-b / div) + " - " + str(math.sqrt(dis * -1) / div) + "i"
+        return [x1, x2]
     else:
-        det = b**2 - 4*a*c
-        d = (abs(det))**(1/2)
-        e = 2*a 
-        x1 = str((b + d)/e) + "i"
-        x2 = str((b - d)/e) + "i"
-        return x1,x2
+        x1 = (- b + math.sqrt(dis)) / div
+        x2 = (- b - math.sqrt(dis)) / div
+        return [x1, x2]
+
         
 print(CuadraticE(1,2,1))
 print(CuadraticE(1,0,1))
