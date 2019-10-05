3f2639e5bcf411c0514b3a80de50209c4cf16f02
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-10-05 11:21:58 -0500

    Create SerieTaylor.py

---
+++ SerieTaylor.py
@@ -0,0 +1,20 @@
+def factorial(x):
+    return 1 if (x < 1) else x * factorial(x-1)
+def potencia(base,exp):
+    if(exp == 0):
+        return 1
+    else:
+        return base * potencia(base,exp-1)
+
+def seno(rad):
+    x = rad * 3.141592 / 180
+    result = 0 
+    for i in range (10):
+        n = (2+i)-1
+        if i%2 == 0:
+            result = result - (potencia(x,n)/factorial(n))
+        else:
+            result = result + (potencia(x,n)/factorial(n))
+    return result * -1
+
+print(seno(3.14661592/2))
