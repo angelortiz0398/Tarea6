bfb3b9db425d69878f9f43386c9b31ef084c046f
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-10-05 11:20:42 -0500

    Create Recursividad.py

---
+++ Recursividad.py
@@ -0,0 +1,8 @@
+def factorial(x):
+    return 1 if (x < 1) else x * factorial(x-1)
+
+num=int(input("Numero: "))
+if num >0 and num < 30:
+    print("el factorial de ",num,"es: ",factorial(num))
+else:
+    print("numero no valido")
