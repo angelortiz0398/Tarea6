e4099c974ef7a23c840b8fc74731d00d6c326303
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-10-17 23:15:58 -0500

    Add files via upload
    Busqueda en un arreglo

---
+++ Tarea 14/LinearBusq.py
@@ -0,0 +1,16 @@
+'''
+LinearBusq.py; busca un elemento en un arreglo de numeros aleatorios
+'''
+import random
+
+def busca(lis,e):
+    for i in range(len(lis)):
+        if lis[i] == e:
+            return i
+    return -1
+
+lista = [random.randint(0,100) for _ in range(10000)]
+print(busca(lista,7))
+print(busca(lista,99))
+print(busca(lista,101))
+print(busca(lista,44))
