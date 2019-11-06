8718ebcd1487f673611d45b34915fe0309ff929e
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2019-11-06 07:58:47 -0600

    Update MultArr.py

--- MultArr.py
+++ MultArr.py
@@ -1,7 +1,10 @@
 def multArr(lis):
     res = 1
-    for i in lis:
-        res *= lis[i-1]
+    if len(lis) != 0:
+        for i in lis:
+            res *= lis[i-1]
+    else:
+        res = 0
     return res     
 lis = [1,2,3,4,5]
 print(multArr(lis))
